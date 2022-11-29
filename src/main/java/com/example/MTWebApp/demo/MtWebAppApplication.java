c336f4e5e157dfd9bd7174672ee8ab73fde2a928
rohitk13-sys <rohitkhatwani13@gmail.com>
2022-11-29 13:14:51 +0530

    Empty project uploaded

---
+++ src/main/java/com/example/MTWebApp/demo/MtWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MTWebApp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MTWebApp/demo/MtWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MTWebApp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
